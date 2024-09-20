6cfc05205fdf79a080e44018173a380d50c4ac52
Olga Gogoleva <mikadoolga@yandex.ru>
2024-09-20 10:14:31 +0300

    change yml, gradle. add generator, test

---
+++ src/test/java/ru/netology/test/AuthTest.java
@@ -0,0 +1,86 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class AuthTest {
+
+    @BeforeEach
+    void setUp() {
+        open("http:localhost:9999");
+    }
+
+    @Test
+    void userPresencePositive() {
+
+        var registredUser = DataGenerator.Registration.generateUser("en", "active");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='login'] input").setValue(registredUser.getLogin());
+        form.$("[data-test-id='password'] input").setValue(registredUser.getPassword());
+        form.$("[data-test-id='action-login']").click();
+        $("h2").shouldHave(Condition.exactText("Личный кабинет")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void userIsBlockedNegative() {
+
+        var registredUser = DataGenerator.Registration.generateUser("en", "blocked");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='login'] input").setValue(registredUser.getLogin());
+        form.$("[data-test-id='password'] input").setValue(registredUser.getPassword());
+        form.$(".button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.exactText("Ошибка! Пользователь заблокирован"))
+                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void invalidLoginNegative() {
+
+        var registredUser = DataGenerator.Registration.generateUser("en", "active");
+        var randomLogin = DataGenerator.Registration.generateLogin("ru");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='login'] input").setValue(randomLogin);
+        form.$("[data-test-id='password'] input").setValue(registredUser.getPassword());
+        form.$(".button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"))
+                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+
+    }
+
+    @Test
+    void invalidPasswordNegative() {
+
+        var registredUser = DataGenerator.Registration.generateUser("en", "active");
+        var randomPassword = DataGenerator.Registration.generatePassword("en");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='login'] input").setValue(registredUser.getLogin());
+        form.$("[data-test-id='password'] input").setValue(randomPassword);
+        form.$(".button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"))
+                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+
+    @Test
+    void unregisteredUser() {
+        var user = DataGenerator.Registration.getUser("en", "active");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id='login'] input").setValue(user.getLogin());
+        form.$("[data-test-id='password'] input").setValue(user.getPassword());
+        form.$(".button").click();
+        $("[data-test-id='error-notification'] .notification__content")
+                .shouldHave(Condition.exactText("Ошибка! Неверно указан логин или пароль"))
+                .shouldBe(Condition.visible, Duration.ofSeconds(15));
+    }
+}
